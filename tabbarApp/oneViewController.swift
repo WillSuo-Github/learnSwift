c0740dc7124a429c3852f1dd7633912bb651ead4
WS <739002611@qq.com>
2016-10-16 20:50:15 +0800

    tabBarApp

---
+++ tabbarApp/oneViewController.swift
@@ -0,0 +1,35 @@
+//
+//  oneViewController.swift
+//  learnSwift
+//
+//  Created by ws on 2016/10/16.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+import UIKit
+
+class oneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
